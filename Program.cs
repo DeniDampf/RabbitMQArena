0d2cb70a3da3bb83a016ac3915d05c68407e84ca
7even <sevensvenson@hotmail.com>
2019-04-13 20:44:38 +0200

    first step

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace RabbitMQArena
 {
@@ -7,6 +9,19 @@ namespace RabbitMQArena
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            ConnectionFactory factory = new ConnectionFactory();
+            
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            factory.VirtualHost = "/";
+            factory.HostName = "localhost";
+            factory.Port = 8080;
+        
+            IConnection conn = factory.CreateConnection();
+
+
         }
     }
 }
